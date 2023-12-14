95b4f6dcab97ab1c900a8e303ac4d9fcd89b41d6
Lord_Meraj <merajp8@gmail.com>
2023-12-15 00:55:47 +0330

    setup BackGround

--- dino.py
+++ dino.py
@@ -1,20 +1,61 @@
+import os
 import sys
 import pygame
 
-WIDTH = 1000
-HEIGHT = 500
+WIDTH = 623
+HEIGHT = 150
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Dino')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dino")
+
+
+class BG:
+    def __init__(self, x):
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.x = x
+        self.y = 0
+        self.set_texture()
+        self.show()
+
+    def update(self, dx):
+        self.x += dx
+        if self.x <= -WIDTH:
+            self.x = WIDTH
+
+    def show(self):
+        screen.blit(self.texture, (self.x, self.y))
+
+    def set_texture(self):
+        path = os.path.join("assets/image/bg.png")
+        self.texture = pygame.image.load(path)
+        self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
+
+
+class Game:
+    def __init__(self):
+        self.bg = [BG(x=0),BG(x=WIDTH)]
+        self.speed = 3
+
 
 def main():
-    #main loop
+    game = Game()
+    
+    clock = pygame.time.Clock()
+    # main loop
     while True:
+        
+        for bg in game.bg:
+            bg.update(-game.speed)
+            bg.show()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        clock.tick(80)
         pygame.display.update()
-        
+
+
 main()
