f50e86377e8e5eb8a14752a41ce9263bb44e425e
Lord_Meraj <merajp8@gmail.com>
2023-12-17 01:16:19 +0330

    show Dino

--- dino.py
+++ dino.py
@@ -10,6 +10,30 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dino")
 
 
+class Dino:
+    def __init__(self):
+        self.width = 44
+        self.height = 44
+        self.x = 10
+        self.y = 80
+        self.texture_num = 0
+        self.set_texture()
+        self.show()
+
+    def update(self, loops):
+        if loops % 4 == 0:
+            self.texture_num = (self.texture_num + 1) % 3
+            self.set_texture()
+
+    def show(self):
+        screen.blit(self.texture, (self.x, self.y))
+
+    def set_texture(self):
+        path = os.path.join(f"assets/image/dino{self.texture_num}.png")
+        self.texture = pygame.image.load(path)
+        self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
+
+
 class BG:
     def __init__(self, x):
         self.width = WIDTH
@@ -35,25 +59,36 @@ class BG:
 
 class Game:
     def __init__(self):
-        self.bg = [BG(x=0),BG(x=WIDTH)]
+        self.bg = [BG(x=0), BG(x=WIDTH)]
+        self.dino = Dino()
         self.speed = 3
 
 
 def main():
     game = Game()
-    
+    dino = game.dino
+
     clock = pygame.time.Clock()
+
+    looops = 0
+
     # main loop
     while True:
-        
+        looops += 1
+        # BG
         for bg in game.bg:
             bg.update(-game.speed)
             bg.show()
+
+        # Dino
+        dino.update(looops)
+        dino.show()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         clock.tick(80)
         pygame.display.update()
 
